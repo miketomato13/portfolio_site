3ecdbc372a80f69d8a5d9d88baaf53dbeaf48aa6
MikeTomato <mike@costabellarealtygroup.com>
2019-04-02 14:41:45 -0400

    removed jquery bloat, testimonials bar, to be rewritten in react eventually

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/header/header';
 import About from './components/about/about';
 import Resume from './components/resume/resume';
 import Portfolio from './components/portfolio/portfolio';
-import Testimonials from  './components/testimonials/testimonials';
 import ContactUs from './components/contactus/contactus';
 import Footer from './components/footer/footer';
 import resumeData from './resumeData';
@@ -15,7 +14,6 @@ class App extends Component {
         <About resumeData={resumeData}/>
         <Resume resumeData={resumeData}/>
         <Portfolio resumeData={resumeData}/>
-        <Testimonials resumeData={resumeData}/>
         <ContactUs resumeData={resumeData}/>
         <Footer resumeData={resumeData}/>
       </div>
--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -13,7 +13,7 @@ export default class Porfolio extends Component {
               return(
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
-                    <a href={`${item.url}`}>
+                    <a href={`${item.url}`} target="_blank">
                       <img src={`${item.imgurl}`} className="item-img"/>
                       <div className="overlay">
                         <div className="portfolio-item-meta">
