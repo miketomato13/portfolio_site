1628168a2de0a25f95d2c842fe298755cabd4cd6
MikeTomato <mike@costabellarealtygroup.com>
2019-04-08 13:56:05 -0400

    changed link order

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -11,9 +11,9 @@ export default class Header extends Component {
           <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+               <li><a className="smoothscroll" href="#portfolio">Works</a></li>
                <li><a className="smoothscroll" href="#about">About</a></li>
              <li><a className="smoothscroll" href="#resume">Resume</a></li>
-               <li><a className="smoothscroll" href="#portfolio">Works</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
             </ul>
          </nav>
